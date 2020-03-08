a159eb1fa32006946d4c41ed0b498a653dcefaaf
Dmitriy Fedorenko <deemon13.85@gmail.com>
2020-03-08 12:46:08 +0200

    Updates

---
+++ precache-manifest.f78e0bfaa4fdc77ba053c92fd4584845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d226a51e8503fdb708386096c47344a3",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "cd254ac456bddb67b2b4",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.072c48d9.chunk.css"
+  },
+  {
+    "revision": "251a2378f99f91071b87",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c83c151c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c83c151c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd254ac456bddb67b2b4",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.698c0782.chunk.js"
+  },
+  {
+    "revision": "29041d2fed37885842fe",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d31d0001.js"
+  }
+]);
